cfd2bcb93a8c4ec930ff1662bf8baf7432ac01b5
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-18 18:03:06 -0400

    Update rendImages.js

--- rendImages.js
+++ rendImages.js
@@ -1,8 +1,24 @@
+var imgIndex = 0;
 const body = d3.select('body');
 const svg = body.append('svg')
     .attr('width', '750')
     .attr('height', '750')
 const img = svg.append('image')
-    .attr('href', '0.tif')
+    .attr('href', '/A_01fld07_brightfield/' + imgIndex + '.png')
     .attr('width', '750')
     .attr('height', '750');
+
+PreviousImage = () => {
+    if (imgIndex > 0) {
+        img.attr('href', '/A_01fld07_brightfield/' + (--imgIndex) + '.png')
+        d3.select('h2').text(imgIndex);
+    }
+}
+NextImage = () => {
+    if (imgIndex < 288) {
+        img.attr('href', '/A_01fld07_brightfield/' + (++imgIndex) + '.png');
+        d3.select('h2').text(imgIndex);
+    }
+}
+
+    
