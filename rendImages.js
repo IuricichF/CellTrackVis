d292b0b7f8cbcf52fb9d78a8fd9a53e8e0204440
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-19 15:40:25 -0400

    Update rendImages.js

--- rendImages.js
+++ rendImages.js
@@ -1,24 +1,24 @@
 var imgIndex = 0;
-var divWidth = 1000;
-var divHeight = 1000;
+const ImageWidth = 1000;
+const ImageHeight = 1000;
 const div = d3.select('div');
 const indexDisplay = div.append('h2')
     .text('Images Index: ' + (imgIndex + 1));
 const svg = div.append('svg')
-    .attr('width', divWidth)
-    .attr('height', divHeight);
+    .attr('width', ImageWidth)
+    .attr('height', ImageHeight);
 const img = svg.append('image')
     .attr('href', '/A_01fld07_brightfield/' + imgIndex + '.png')
-    .attr('width', divWidth)
-    .attr('height', divHeight);
+    .attr('width', ImageWidth)
+    .attr('height', ImageHeight);
 
-PreviousImage = () => {
+const PreviousImage = () => {
     if (imgIndex > 0) {
         img.attr('href', '/A_01fld07_brightfield/' + (--imgIndex) + '.png')
         indexDisplay.text('Images Index: ' + (imgIndex + 1));
     }
 }
-NextImage = () => {
+const NextImage = () => {
     if (imgIndex < 288) {
         img.attr('href', '/A_01fld07_brightfield/' + (++imgIndex) + '.png');
         indexDisplay.text('Images Index: ' + (imgIndex + 1));
