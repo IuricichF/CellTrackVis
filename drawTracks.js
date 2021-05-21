11764006095914276e51b57d1f8fe3c9fee63c3d
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-20 20:48:45 -0400

    Update drawTracks.js

--- drawTracks.js
+++ drawTracks.js
@@ -1,21 +1,20 @@
 const RADIUS = 5;
 const RESOLU_WIDTH = 2040;
 const RESOLU_HEIGHT = 2040;
-const RESOLU_WIDTH_RATIO = ImageWidth / RESOLU_WIDTH;
-const RESOLU_HEIGHT_RATIO = ImageHeight / RESOLU_HEIGHT;
-const UpdateTracks = () => {
-    if (imgIndex % 3 == 0) {
-        svg.selectAll('circle').remove();
-        d3.csv('/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(data => {
-            data.filter(d => d.FRAME == imgIndex / 3)
-                .forEach(d => {
-                    svg.append('circle')
-                        .attr('r', RADIUS)
-                        .attr('fill', '#ff0008')
-                        .attr('cx', d.pos_x * RESOLU_WIDTH_RATIO)
-                        .attr('cy', d.pos_y * RESOLU_HEIGHT_RATIO);
-                })
+const RESOLU_WIDTH_RATIO = IMAGE_WIDTH / RESOLU_WIDTH;
+const RESOLU_HEIGHT_RATIO = IMAGE_HEIGHT / RESOLU_HEIGHT;
+var data = null;
+d3.csv('/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(csv => { data = csv });
+let previousIndex = 0;
+function updateTracks() {
+    data.filter(d => d.FRAME <= imgIndex && d.FRAME > previousIndex)
+        .forEach(d => {
+            svg.append('circle')
+                .attr('id', d.FRAME)
+                .attr('r', RADIUS)
+                .attr('fill', '#ff0008')
+                .attr('cx', d.pos_x * RESOLU_WIDTH_RATIO + IMAGE_WIDTH / 2)
+                .attr('cy', d.pos_y * RESOLU_HEIGHT_RATIO );
         })
-    }
+    previousIndex = imgIndex;
 }
-UpdateTracks();
