ed2bf0964621ba0434d748a8adca12527bf63897
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-21 13:30:29 -0400

    Update drawTracks.js

--- drawTracks.js
+++ drawTracks.js
@@ -7,7 +7,7 @@ var data = null;
 d3.csv('/DataVis/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(csv => { data = csv });
 let previousIndex = 0;
 function updateTracks() {
-    data.filter(d => d.FRAME <= imgIndex && d.FRAME > previousIndex)
+    data.filter(d => d.FRAME <= imgIndex  / 4&& d.FRAME > previousIndex / 4)
         .forEach(d => {
             svg.append('circle')
                 .attr('id', d.FRAME)
