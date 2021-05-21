2dadcabc64d68b3cf0ba113043c381c1d4a1d1a2
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-21 13:21:50 -0400

    Update drawTracks.js

--- drawTracks.js
+++ drawTracks.js
@@ -4,7 +4,7 @@ const RESOLU_HEIGHT = 2040;
 const RESOLU_WIDTH_RATIO = IMAGE_WIDTH / RESOLU_WIDTH;
 const RESOLU_HEIGHT_RATIO = IMAGE_HEIGHT / RESOLU_HEIGHT;
 var data = null;
-d3.csv('/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(csv => { data = csv });
+d3.csv('/DataVis/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(csv => { data = csv });
 let previousIndex = 0;
 function updateTracks() {
     data.filter(d => d.FRAME <= imgIndex && d.FRAME > previousIndex)
