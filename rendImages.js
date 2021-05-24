74d06c847043719b661356c3c6e200a42197ac9b
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-24 15:18:08 -0400

    Update rendImages.js

--- rendImages.js
+++ rendImages.js
@@ -5,47 +5,27 @@ const IMAGE_WIDTH = 1000;
 const IMAGE_HEIGHT = 1000;
 const SVG_PERCENT = 75;
 const NUM_IMAGE = 289
-const div = d3.select('div')
+const div = d3.select("div")
 // div
-const indexDisplay = div.append('h2')
-    .text('Images Index: ' + (imgIndex + 1));
+const indexDisplay = div.append("h2")
+    .text("Images Index: " + (imgIndex + 1));
 // svg
-const svg = div.append('svg')
-    .attr('width', SVG_PERCENT + '%')
-    .attr('height', SVG_PERCENT + '%')
-    .attr('viewBox', '0 0 ' + VIEW_BOX_WIDTH + ' ' + VIEW_BOX_HEIGHT);
+const svg = div.append("svg")
+    .attr("width", SVG_PERCENT + "%")
+    .attr("height", SVG_PERCENT + "%")
+    .attr("viewBox", "0 0 " + VIEW_BOX_WIDTH + " " + VIEW_BOX_HEIGHT);
 // image group
-const imgGroup = svg.append('g')
-    .attr('transform', 'translate(' + IMAGE_WIDTH / 2 + ', 0)');
+const imgGroup = svg.append("g")
+    .attr("transform", "translate(" + IMAGE_WIDTH / 2 + ", 0)");
 // rend image
-const img = imgGroup.append('image')
+const img = imgGroup.append("image")
     .attr('href', '/DataVis/A_01fld07_brightfield/' + imgIndex + '.png')
-    .attr('width', IMAGE_WIDTH)
-    .attr('height', IMAGE_HEIGHT);
-// scroll bar group
-const scrollBarGroup = imgGroup.append('g')
-    .attr('transform', 'translate(0, ' + IMAGE_HEIGHT * 1.01 + ')')
-// scroll bar container
-scrollBarGroup.append('rect')
-    .attr('id', 'scrollBarContainer')
-    .attr('stroke', 'black')
-    .attr('fill', '#a19f99')
-    .attr('width', IMAGE_WIDTH)
-    .attr('height', IMAGE_HEIGHT / 10);
-// draggable control object
-const draggableObjWidth = IMAGE_WIDTH / NUM_IMAGE + 10;
-scrollBarGroup.append('rect')
-    .attr('id', 'draggableObj')
-    .attr('stroke', 'black')
-    .attr('fill', 'white')
-    .attr('width', draggableObjWidth)
-    .attr('height', IMAGE_HEIGHT / 10);
+    .attr("width", IMAGE_WIDTH)
+    .attr("height", IMAGE_HEIGHT);
 // function
 function updateImage(newIndex) {
-    if (newIndex != imgIndex) {
-        imgIndex = newIndex;
-        img.attr('href', '/DataVis/A_01fld07_brightfield/' + imgIndex + '.png');
-        updateTracks();
-        indexDisplay.text('Images Index: ' + (imgIndex + 1));
-    }
+    imgIndex = newIndex;
+    img.attr('href', '/DataVis/A_01fld07_brightfield/' + imgIndex + '.png');
+    drawTracks();
+    indexDisplay.text("Images Index: " + (imgIndex + 1));
 }
