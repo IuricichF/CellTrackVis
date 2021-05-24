f570cbc20cbf2a586794f85cc58d9aaae6c73379
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-05-24 15:23:03 -0400

    Update drawTracks.js

--- drawTracks.js
+++ drawTracks.js
@@ -1,20 +1,69 @@
-const RADIUS = 5;
+var START_COLOR = "#ff0000";
+const TRACK_WIDTH = 4;
 const RESOLU_WIDTH = 2040;
 const RESOLU_HEIGHT = 2040;
 const RESOLU_WIDTH_RATIO = IMAGE_WIDTH / RESOLU_WIDTH;
 const RESOLU_HEIGHT_RATIO = IMAGE_HEIGHT / RESOLU_HEIGHT;
-var data = null;
-d3.csv('/DataVis/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv').then(csv => { data = csv });
-let previousIndex = 0;
-function updateTracks() {
-    data.filter(d => d.FRAME <= imgIndex  / 4&& d.FRAME > previousIndex / 4)
-        .forEach(d => {
-            svg.append('circle')
-                .attr('id', d.FRAME)
-                .attr('r', RADIUS)
-                .attr('fill', '#ff0008')
-                .attr('cx', d.pos_x * RESOLU_WIDTH_RATIO + IMAGE_WIDTH / 2)
-                .attr('cy', d.pos_y * RESOLU_HEIGHT_RATIO );
-        })
-    previousIndex = imgIndex;
+const scaleX = d3.scaleLinear()
+    .domain([0, RESOLU_WIDTH])
+    .range([IMAGE_WIDTH / 2, RESOLU_WIDTH * RESOLU_WIDTH_RATIO + IMAGE_WIDTH / 2])
+const scaleY = d3.scaleLinear()
+    .domain([0, RESOLU_HEIGHT])
+    .range([0, RESOLU_HEIGHT * RESOLU_HEIGHT_RATIO])
+
+function drawTracks() {
+    var trackPaths = [];
+    // get track data
+    d3.csv("/DataVis/A_01fld07_brightfield/a_01fld07_05-09-2021-12-48-25.csv").then(trackData => {
+        // scale imgIndex to frame
+        const scaleImageIndex = d3.scaleLinear()
+            .domain([0, NUM_IMAGE - 1])
+            .range([0, Math.max(...trackData.map(d => d.FRAME))])
+        const imgIndexScaled = Math.trunc(scaleImageIndex(imgIndex));
+        // get trackData of frame from 0 to current 
+        const trackDataToCurr = trackData.filter(d => d.FRAME <= imgIndexScaled);
+        // get the number of tracks
+        const maxTracks = Math.max(...trackDataToCurr.map(d => d.TRACK_ID)) + 1;
+        for (let i = trackPaths.length; i < maxTracks; i++) trackPaths[i] = [];
+        // get tracks info into trackPaths
+        trackDataToCurr.filter(d => d.FRAME < imgIndexScaled)
+            .forEach(d => {
+                trackPaths[d.TRACK_ID].push([scaleX(d.pos_x), scaleY(d.pos_y)]);
+                trackPaths[d.TRACK_ID].push([scaleX(+d.pos_x + +d.dt1_n0_dx), scaleY(+d.pos_y + +d.dt1_n0_dy)]);
+                trackPaths[d.TRACK_ID].push([scaleX(+d.pos_x + +d.dt2_n0_dx), scaleY(+d.pos_y + +d.dt2_n0_dy)]);
+            });
+        trackDataToCurr.filter(d => d.FRAME == imgIndexScaled)
+            .forEach(d => {
+                trackPaths[d.TRACK_ID].push([scaleX(d.pos_x), scaleY(d.pos_y)]);
+                if (scaleImageIndex.invert(imgIndexScaled) + 1 == imgIndex) {
+                    trackPaths[d.TRACK_ID].push([scaleX(+d.pos_x + +d.dt1_n0_dx), scaleY(+d.pos_y + +d.dt1_n0_dy)]);
+                    if (scaleImageIndex.invert(imgIndexScaled) + 2 == imgIndex) {
+                        trackPaths[d.TRACK_ID].push([scaleX(+d.pos_x + +d.dt2_n0_dx), scaleY(+d.pos_y + +d.dt2_n0_dy)]);
+                    }
+                }
+            });
+
+        // build start points
+        svg.selectAll("circle")
+            .data(trackPaths)
+            .attr("cx", d => { if (d[0] != undefined) return +d[0][0] })
+            .attr("cy", d => { if (d[0] != undefined) return +d[0][1] })
+            .enter()
+            .append("circle")
+            .attr("cx", d => { if (d[0] != undefined) return +d[0][0] })
+            .attr("cy", d => { if (d[0] != undefined) return +d[0][1] })
+            .attr("r", TRACK_WIDTH)
+            .attr('fill', START_COLOR)
+        // build paths
+        svg.selectAll("path")
+            .data(trackPaths)
+            .attr("d", d => d3.line()(d))
+            .enter()
+            .append("path")
+            .attr("d", d => d3.line()(d))
+            .attr("fill", "none")
+            .attr("stroke", START_COLOR)
+            .attr("stroke-width", TRACK_WIDTH);
+    });  
 }
+drawTracks();
