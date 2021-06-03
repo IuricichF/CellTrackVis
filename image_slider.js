defad3ac1e427d221f0cb9dc50eee70faef1d53a
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-06-03 18:42:42 -0400

    Update image_slider.js

--- image_slider.js
+++ image_slider.js
@@ -1,13 +1,5 @@
 // image slider
 const IMG_SLD_EL = document.getElementById("image_slider");
-// left, right keys to change image index
-window.addEventListener("keydown", e => {
-    if (e.keyCode == "37") {
-        // left arrow
-        if (+IMG_SLD_EL.value > +IMG_SLD_EL.min) updateImage(--IMG_SLD_EL.value);
-    }
-    else if (e.keyCode == "39") {
-        // right arrow
-        if (+IMG_SLD_EL.value < +IMG_SLD_EL.max) updateImage(++IMG_SLD_EL.value);
-    }
-});
+const IMG_SLD_TXT = d3.select("#imageSliderContainer").append("label")
+    .attr("for", "#image_slider")
+    .text(`Image Index: ${IMG_SLD_EL.value}`);
