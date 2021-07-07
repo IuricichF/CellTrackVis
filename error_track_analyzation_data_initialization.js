d539a91db9147a6bb32643dcfe0514f9684db7b2
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-07-07 15:03:15 -0400

    Delete error_track_analyzation_data_initialization.js

--- error_track_analyzation_data_initialization.js
+++
@@ -1,9 +0,0 @@
-const trkData = JSON.parse(localStorage.getItem("trkDataSortedByTrkID")).find(d =>
-    d[0].trkID === JSON.parse(localStorage.getItem("setTrkToAnalyseArr"))[+localStorage.getItem("numPageToAnalyse") - 1]
-)
-const idxToTrkIDPredArr = new Map(JSON.parse(localStorage.getItem("trkIDToTrkIDPredMap"))).get(trkData[0].trkID)
-const trkPredData = JSON.parse(localStorage.getItem("trkDataSortedByTrkIDPred")).filter(d =>
-    idxToTrkIDPredArr.includes(d[0].trkIDPred)
-)
-const datasetIdx = localStorage.getItem("datasetIdx");
-const numImg = localStorage.getItem("numImg");
