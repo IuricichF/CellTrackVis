a4decdbe630e2936796d7bfde4f62dc2a1241cb8
Lygihub <56136705+Lygihub@users.noreply.github.com>
2021-09-12 21:07:52 -0400

    Update index_data_initialization.js

--- index_data_initialization.js
+++ index_data_initialization.js
@@ -4,7 +4,7 @@ var numDataset;
 function initializeData(numD) {
     numDataset = numD
     for (let i = 0; i < numDataset; i++) {
-        d3.csv(`/src/dataset_${i + 1}/track_data.csv`).then(rawData => {
+        d3.csv(`/DataVis/src/dataset_${i + 1}/track_data.csv`).then(rawData => {
             let tempNumImg = +rawData[rawData.length - 1].FRAME * 4 + 1;
             let tempTrkData = [];
             let tempTrkDataSortedByTrkID = [];
